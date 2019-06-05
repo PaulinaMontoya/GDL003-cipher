2e22e581a2e8915ec3715dd023d9922d699372cb
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-06-05 18:55:59 -0500

    Sale texto decifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,36 @@
 window.cipher = {
-  // ... 
-};
+  encode: function(toCodi, shift){
+
+let x = toCodi.value;
+let result = "";
+for (let i = 0; i < x.length; i++) {
+          let c = x[i].charCodeAt(0);
+              if(c >= 97 && c <=  122) {
+            let form = (c - 65 + parseInt(shift.value)) % 26 + 65;
+            let codificado = String.fromCharCode(form);
+            result += codificado;
+        }
+     }
+   return result;
+  }
+}
+
+
+/*
+
+FORMULA FLORE
+
+  encode: function(toCodi, shift){
+let x = toCodi.value;
+let result = "";
+for (let i = 0; i < x.lenght; i++) {
+          let c = x[i].charCodeAt(0);
+              if(c >= 65 && c <=  90) {
+             let form = ((c - 65 + parseInt(shift.value)) % 26 + 65);
+             result =  x[i].fromCharCode(form);
+        }
+     }
+   return result;
+  }
+}
+*/
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-
-
-
 let nameUser = document.getElementById("userbox");
+let toCodi = document.getElementById("codibox");
+let toDeco = document.getElementById("decobox");
+let shift = document.getElementById("numberday");
+const showCodi = document.getElementById("buttoncodi");
+
 
   first.style.display = "block";
   second.style.display = "none";
@@ -33,7 +35,13 @@ function enter() {
     fourth.style.display = "block";
     fifth.style.display = "none";
     document.getElementById("usernamecodi").innerHTML = nameUser.value;
-   }
+      document.getElementById("coditext").innerHTML=window.cipher.encode(toCodi, shift);
+
+  //  document.write(window.cipher.encode());
+//    showCodi.addEventListener("click",function(){document.getElementById("coditext").innerHTML =(window.cipher.encode())});
+}
+
+
 
   function nextdeco() {
     first.style.display = "none";
@@ -42,6 +50,7 @@ function enter() {
     fourth.style.display = "none";
     fifth.style.display = "block";
     document.getElementById("usernamedeco").innerHTML = nameUser.value;
+    document.getElementById("decotext").innerHTML = toDeco.value;
    }
 
   function close() {
