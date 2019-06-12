7fbe08b689a98f276904ae5c7ed19a9707832301
PaulinaMontoya <paulinamontoya90@gmail.com>
2019-06-12 17:23:34 -0500

    Funcionan cambios 12 Junio

--- src/cipher.js
+++ src/cipher.js
@@ -1,66 +1,6 @@
 window.cipher = {
-<<<<<<< HEAD
-  
-  encode:(toCodi, shift) => {
-=======
 
-  encode: (shift, toCodi) => {
-
-  let result = "";
-  for (let i = 0; i < toCodi.length; i++) {
-        let c = toCodi[i].charCodeAt(0);
-            if(c >= 97 && c <=  122) {
-              let lower = (c - 97 + parseInt(shift)) % 26 + 97;
-              let codificado = String.fromCharCode(lower);
-              result += codificado;
-        }
-     }
-   return result;
-  },
-
-  decode:(shift, toCodi) => {
-
-    let result = "";
-    for (let i = 0; i < toDeco.length; i++) {
-          let c = toDeco[i].charCodeAt(0);
-          if(c >= 97 && c <=  122) {
-                let lower = (c + 97 - parseInt(shift) +14 ) % 26 + 97;
-                let decodificado = String.fromCharCode(lower);
-                result += decodificado;
-          }
-       }
-     return result;
-    }
-};
-
-
-/*
-PARA AGREGAR ESPACIOS
-else if(c == 32) {
-  let space = " ";
-result = c  space;
-}
-
-PARA MAYUSCULAS CIFRADO
-else if(c >= 65 && c <=  90) {
-  let upper = (c - 65 + parseInt(shift)) % 26 + 65;
-  let codificado = String.fromCharCode(upper);
-  result += codificado;
-}
-
-PARA MAYUSCULAS DECIFRADO
-else if(c >= 65 && c <=  90) {
-  let upper = (c + 65 - parseInt(shift)) % 26 + 65;
-  let decodificado = String.fromCharCode(upper);
-  result += decodificado;
-  }
-*/
-
-/*
-
-window.cipher = {
   encode: function encode(toCodi, shift){
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
 
   let x = toCodi.value;
   let result = "";
@@ -75,11 +15,7 @@ window.cipher = {
    return result;
   },
 
-<<<<<<< HEAD
-  decode:(toDeco, shift) => {
-=======
   decode: function decode(toDeco, shift){
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
 
     let x = toDeco.value;
     let result = "";
@@ -94,11 +30,3 @@ window.cipher = {
      return result;
     }
 };
-<<<<<<< HEAD
-
-/*
-console.log (x);
-=======
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
-
-*/
--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 let nameUser = document.getElementById("userbox");
-<<<<<<< HEAD
 let toCodi = document.getElementById("codibox");
 let toDeco = document.getElementById("decobox");
 let shift = document.getElementById("numberday");
-=======
-let toCodi = document.getElementById("codibox").value;
-let toDeco = document.getElementById("decobox").value;
-let shift = document.getElementById("numberday").value;
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
 let first = document.getElementById("first");
 let second = document.getElementById("second");
 let third = document.getElementById("third");
 let fourth = document.getElementById("fourth");
 let fifth = document.getElementById("fifth");
-
 
   first.style.display = "block";
   second.style.display = "none";
@@ -21,65 +14,32 @@ let fifth = document.getElementById("fifth");
   fourth.style.display = "none";
   fifth.style.display = "none";
 
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
 const enter = () => {
   first.style.display = "none";
   second.style.display = "block";
   third.style.display = "none";
   fourth.style.display = "none";
   fifth.style.display = "none";
-<<<<<<< HEAD
  };
 
-
-
- const nextday = () => {
-=======
-};
-
-
 const nextday = () => {
-
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
    first.style.display = "none";
    second.style.display = "none";
    third.style.display = "block";
    fourth.style.display = "none";
    fifth.style.display = "none";
-<<<<<<< HEAD
   };
 
-
-
-  const nextcodi = () => {
-=======
- };
-
-
  const nextcodi = () => {
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
     first.style.display = "none";
     second.style.display = "none";
     third.style.display = "none";
     fourth.style.display = "block";
     fifth.style.display = "none";
     document.getElementById("usernamecodi").innerHTML = nameUser.value;
-<<<<<<< HEAD
     document.getElementById("coditext").innerHTML = window.cipher.encode(toCodi, shift);
   };
 
-
-
-=======
-    document.getElementById("coditext").innerHTML = window.cipher.decode(toCodi, shift);
-  };
-
-
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
   const nextdeco = () => {
     first.style.display = "none";
     second.style.display = "none";
@@ -87,18 +47,9 @@ const nextday = () => {
     fourth.style.display = "none";
     fifth.style.display = "block";
     document.getElementById("usernamedeco").innerHTML = nameUser.value;
-<<<<<<< HEAD
     document.getElementById("decotext").innerHTML =  window.cipher.decode(toDeco, shift);
    };
 
-
-
-=======
-    document.getElementById("decotext").innerHTML =  window.cipher.encode(toDeco, shift);
-  };
-
-
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
   const backfirst = () => {
     first.style.display = "block";
     second.style.display = "none";
@@ -107,18 +58,10 @@ const nextday = () => {
     fifth.style.display = "none";
   };
 
-<<<<<<< HEAD
   let buttonEnter = document.getElementById("buttonenter");
   buttonEnter.addEventListener("click", enter);
   let buttonDay = document.getElementById("buttonday");
   buttonDay.addEventListener("click", nextday);
-=======
-
-  let buttonEnter = document.getElementById("buttonenter");
-  buttonEnter.addEventListener("click", enter);
-  let buttonNextday = document.getElementById("nextday");
-  buttonNextday.addEventListener("click", nextday);
->>>>>>> 61638630c2f50402942dd8685baa116f5ef150af
   let buttonCodi = document.getElementById("buttoncodi");
   buttonCodi.addEventListener("click", nextcodi);
   let buttonDeco = document.getElementById("buttondeco");
